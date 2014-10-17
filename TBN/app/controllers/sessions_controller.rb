fc7b5dd57c0d5393b973bee4c17f2f62b8d25578
Brian <bajunio@gmail.com>
2014-10-17 14:36:17 -0700

    add authenticate to user model

--- TBN/app/controllers/sessions_controller.rb
+++ TBN/app/controllers/sessions_controller.rb
@@ -1,7 +1,22 @@
 class SessionsController < ApplicationController
 
+def new
+end
+
 def create
-  redirect_to root_path
+  user = User.authenticate(params[:user_name], params[:password])
+  if user
+    session[:user_id] = user.id
+    redirect_to root_path, :notice => "Logged in!"
+  else
+    flash.now.alert = "Invalid email or password"
+    render "new"
+  end
+end
+
+def destroy
+  session[:user_id] = nil
+  redirect_to root_url, :notice => "Logged out!"
 end
 
 end
