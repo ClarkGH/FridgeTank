d3e3561f99bce6b2af38ebe492025676d29bc0e2
djohnson0610 <djohnson0610@hotmail.com>
2014-10-16 22:21:42 -0700

    grocery list model changed to make grocery_list_ingredient associations dependent on lists. List destroyed => associations destroyed.

--- TBN/app/models/grocery_list.rb
+++ TBN/app/models/grocery_list.rb
@@ -1,6 +1,6 @@
 class GroceryList < ActiveRecord::Base
 	
-	has_many :grocery_list_ingredients
+	has_many :grocery_list_ingredients, :dependent => :destroy
 	has_many :ingredients, through: :grocery_list_ingredients
 
 	belongs_to :user
