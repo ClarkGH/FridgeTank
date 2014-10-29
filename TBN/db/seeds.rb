36495187e83b242ec1d84a849fc0d0c090a62803
djohnson0610 <djohnson0610@hotmail.com>
2014-10-29 13:45:35 -0700

    Seed file created with three users. Each user has one list with two ingredients

--- TBN/db/seeds.rb
+++ TBN/db/seeds.rb
@@ -5,4 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-GroceryList.create(name: "Football Game Foodz")
+devin = User.create(email: "devin@example.com", password: "password")
+clark = User.create(email: "clark@example.com", password: "password")
+brian = User.create(email: "brian@example.com", password: "password")
+
+devin.grocery_lists << GroceryList.create(name: "Video Game Grub")
+clark.grocery_lists << GroceryList.create(name: "Fat Man Food")
+brian.grocery_lists << GroceryList.create(name: "Football Game Foodz")
+
+devin.grocery_lists.first.ingredients << Ingredient.create(name: "Doritos")
+devin.grocery_lists.first.ingredients << Ingredient.create(name: "Mountain Dew")
+
+clark.grocery_lists.first.ingredients << Ingredient.create(name: "Meatloaf")
+clark.grocery_lists.first.ingredients << Ingredient.create(name: "Twinkies")
+
+brian.grocery_lists.first.ingredients << Ingredient.create(name: "Chicken Wings")
+brian.grocery_lists.first.ingredients << Ingredient.create(name: "Cheese")
