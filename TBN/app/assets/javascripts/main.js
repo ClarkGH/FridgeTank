b7deae8b516b7d81e4a8f022ba5f960bbbfa4591
djohnson0610 <djohnson0610@hotmail.com>
2014-10-29 11:58:00 -0700

    Users lists are appended to body after get-lists AJAX call.

--- TBN/app/assets/javascripts/main.js
+++ TBN/app/assets/javascripts/main.js
@@ -10,9 +10,18 @@ function bindEvents(){
       url: '/lists',
       dataType: 'json'
     }).success(function(response){
-      console.log(response)
+      appendLists(response)
     }).error(function(response){
       console.log('Something wicked this way comes...')
     })
   })
+}
+
+function appendLists(lists){
+  for (var i = 0; i < lists.length; i++){
+    var list = $("<div class='list'></div>");
+    var listName = $("<h1 class='list-name'><a href='/lists/"+lists[i].id+"'>"+lists[i].name+"</a></h1>");
+    list.append(listName);
+    $('body').append(list);
+  }
 }
